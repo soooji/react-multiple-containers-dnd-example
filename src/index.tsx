d8cb1c3fba686ef207db0024e85605ac4814e8c2
Sajad Beheshti <sajadtrackman@gmail.com>
2022-09-05 11:37:58 +0000

    root changed

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from "react";
-import * as ReactDOMClient from "react-dom/client";
+
+import ReactDOM from 'react-dom';
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = ReactDOMClient.createRoot(rootElement);
 
-root.render(
+ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>
-);
+  , document.getElementById('root'));
