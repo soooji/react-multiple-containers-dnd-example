cbbe0bd3a826f923553f8bfb723aa844a62ea499
Sajad Beheshti <sajadtrackman@gmail.com>
2022-09-05 11:41:20 +0000

    Packages updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,8 @@
-import { StrictMode } from "react";
-
 import ReactDOM from 'react-dom';
 
 import App from "./App";
 
 
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <App />
   , document.getElementById('root'));
